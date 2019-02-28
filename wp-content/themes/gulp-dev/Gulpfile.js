60a248a1d2f1b6f8fd0ec79bd387cec9550476a5
ToriStutzman <sstuutzzmaan@gmail.com>
2019-02-28 12:51:10 -0800

    Gulp Dev install

--- wp-content/themes/gulp-dev/Gulpfile.js
+++ wp-content/themes/gulp-dev/Gulpfile.js
@@ -1,4 +1,4 @@
-var themename = 'humescores';
+var themename = 'trinity';
 
 var gulp = require('gulp'),
 	// Prepare and optimize code etc
@@ -58,7 +58,7 @@ gulp.task('javascript', function() {
 gulp.task('watch', function() {
 	browserSync.init({ 
 		open: 'external',
-		proxy: 'humescores.dev',
+		proxy: 'localhost/test',
 		port: 8080
 	});
 	gulp.watch([root + '**/*.css', root + '**/*.scss' ], ['css']);
